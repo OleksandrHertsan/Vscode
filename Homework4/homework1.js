9572f80d4ab7ffcf4c762f69437e9699835abfbb
Alex <alexgercan88@gmail.com>
2022-11-28 22:26:36 +0200

    asd

---
+++ Homework4/homework1.js
@@ -0,0 +1,86 @@
+// let d = 7
+// console.log(d);
+// console.log(d**2);
+// console.log(d**3);
+
+
+// let num = +prompt("Enter the number");
+// if(num > 0){
+//     console.log(num * 2)
+// }
+// else{
+//     console.log("Error")
+// }
+
+
+// let num = +prompt("Enter the number");
+// if(num > 0){
+//     console.log("Положительное")
+// }
+// else if(num < 0){
+// console.log("Отрицательное")
+// }
+// else{
+//     console.log("Null")
+// }
+
+// let num1 = +prompt("Enter the num1")
+// let num2 = +prompt("Enter the num2")
+// if(num1 > num2){
+//     console.log(num1 + num2)
+// }
+// else if (num2 > num1){
+//     console.log(num2 * num1)
+// }
+// else{
+//     console.log("Числа одинаковые")
+// }
+
+// let num = +prompt("Enter the number");
+// if(num < 0){
+//     console.log(num ** 2);
+// }
+// else{
+//     console.log("ERROR")
+// }
+
+// let num = +prompt("Enter the number of students");
+// let num2 = +prompt("Enter the number of chair");
+// if(num2 > num){
+//     console.log("Стульев хватает")
+// }
+// else{
+//     console.log("Стульев не хватает")
+// }
+
+// console.log("Start")
+// for(let num = 0; num < 10; num++){
+//     console.log(num)
+// }
+// console.log("Finish")
+
+// for(let num = 10; num < 100; num+=5){
+//     if (num % 5 == 0);{
+//         console.log(num);
+// }
+// }
+
+
+// let num = Number(prompt('Enter the number'));
+// let num1 = num + 14;
+// let num2 = 0;
+// for(i = num; i < num1; i++){
+//     if(i % 2 != 0){
+//         num2 = i;
+//         console.log(num2);
+//     }
+// }
+
+let arr = [2, 18, 23, 19, 27, 14, 8];
+for(let num = arr; num >= 15; num++){
+    if(arr[num] >= 15){
+        
+    }
+    
+}
+console.log(arr);
---
+++ Lesson 7/script.js
@@ -0,0 +1,118 @@
+// alert('Hello world');
+
+// console.log('Hello world');
+
+// let first_name = "Alex"; //string
+
+// let num = 25; //number
+
+// let question = true; //boolean
+
+// num = 10;
+
+// const pi = 3.14;
+
+// let result = prompt("Enter the number");
+
+// console.log(typeof(true));
+
+//let num = 12.34
+
+// let num = 25;
+
+// let str1 = "Hello";
+// let str2 = 'Hello';
+// let str3 = `Hello ${num}`;
+
+// console.log(2 + 3);
+// console.log(4 - 2);
+// console.log(2 * 4);
+// console.log(6 / 2);
+// console.log(5 ** 2);
+// console.log(11 % 2);
+
+// console.log("Oleks" + "Hertsan");
+
+// let first_name = "Oleks";
+// let last_name = "Hertsan";
+
+// console.log(first_name + " " + last_name);
+
+// let first_name = "Oleks";
+// let greeting = "Hello";
+
+// console.log = (`${greeting}, ${first_name}`);
+
+// let r = 12;
+// let g = 34;
+// let b = 14;
+// console.log("rgb"(" "))
+
+// let num = 123;
+// let str = String(num)
+
+// console.log(num)
+// console.log(typeof(num));
+// console.log(typeof(str));
+
+// let str = "123";
+//let num = Number(str);
+
+// let str ="123";
+// let num = +str;
+
+//console.log('3' + 2); //32
+//console.log('3' * 2;) //6
+//console.log('6' / 2); //3
+//console.log('6' - 1); //5
+//console.log(+'3' + 2); //5
+
+//let age = +prompt("Enter your age", "years")
+
+// let number = +prompt("Enter a number");
+// number = Number(number);
+// console.log(num ** 2);
+
+// let num1 = +prompt("Enter number 1")
+// let num2 = +prompt("Enter number 2")
+// console.log(num1 + num2)
+
+// if(условие){
+//     тело условия
+// }
+
+// let num = Number(prompt("Enter the number"));
+
+// if(num == 0){
+//     console.log("Null");
+// }
+// else if (num > 0){
+//     console.log("Positive")
+// }
+// else{
+//     console.log("Negative");
+// }
+
+// Строгое равенство
+// if("true" === true){
+//     console.log("True");
+// }
+// let num1 = +prompt("Enter the number 1");
+// let num2 = +prompt("Enter the nuber 2");
+// if(num1 > num2){
+//     console.log("Nubmer 1 biggest");
+// }
+// else{
+//     console.log("Number 2 biggest")
+// }
+
+// let num1 = +prompt("Enter the number");
+// if(num1 == 0){
+//     console.log("Number = 0")
+// }
+// else if(num1 % 2 == 0){
+//     console.log("Its even number")
+// }
+// else{
+//     console.log("Its odd number")
+// }
